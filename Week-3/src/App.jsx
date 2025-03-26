3d2d7ccc8f1e6bbbc08d5560402bea9bb5bdccfd
mtanyaa <maheshwaritanya2810@gmail.com>
2025-03-26 22:32:09 +0530

    week3 done

---
+++ Week-3/src/App.jsx
@@ -0,0 +1,31 @@
+import TextUpdater from './components/TextUpdater.jsx'
+import './App.css'
+import Form from './components/Form.jsx'
+import UserCard from "./components/UserCard";
+import Button from './components/Button.jsx';
+import LoginForm from './components/LoginForm.jsx';
+function App() {
+
+  return (
+    <>
+
+      {/* <TextUpdater/> */}
+
+
+      {/* <Form/> */}
+
+      {/* <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <UserCard name="John Doe" email="john.doe@example.com" />
+      <UserCard name="Jane Smith" email="jane.smith@example.com" />
+    </div> */}
+
+
+      {/* <Button/> */}
+
+
+      {/* <LoginForm/> */}
+    </>
+  )
+}
+
+export default App
