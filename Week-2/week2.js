3402dd8404f78f5fd4517f80bfcd5ff653014aac
mtanyaa <maheshwaritanya2810@gmail.com>
2025-03-26 22:30:54 +0530

    week2 done

---
+++ Week-2/week2.js
@@ -0,0 +1,28 @@
+// 1. Declare variables
+let age = 25;
+let isAdult = age >= 18;
+
+// 2. Arithmetic operations
+let x = 10, y = 5;
+let sum = x + y;
+let product = x * y;
+let remainder = x % y;
+
+// 3. Check even or odd
+function evenOdd(num) {
+    return num % 2 === 0;
+}
+evenOdd(7);
+
+// 4. Store numbers 1 to 5 in an array
+let numbers = [];
+for (let i = 1; i <= 5; i++) {
+    numbers.push(i);
+}
+
+// 5. Function to return square of a number
+function square(num) {
+    return num * num;
+}
+
+
